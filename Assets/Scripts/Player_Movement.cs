98d748e4e12665d3d851ad0135bcbdfd74e7b315
sbeeve2023 <cgoss3@my.bcit.ca>
2025-10-03 18:58:07 -0700

    Added player animations in

--- Assets/Scripts/Player_Movement.cs
+++ Assets/Scripts/Player_Movement.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    //player movement
     public float speed;
     private Rigidbody2D rb2d;
+    //movement animation
+    public Animator animator;
+
+    //Firing bullet info
     public float fireRate = 0.25f;
     private float nextFireTime = 0f;
-
     public GameObject bulletPrefab;   
     public float bulletSpeed;
+
+    //Player health
+    public float player_health = 100f;
+
+    //Melee Attack info
+    public float meleeRange = 1f;   // distance in front of player
+    public float meleeRadius = 0.5f; // size of hitbox
+    public int damage = 1;
+    public float attackRate = 0.5f;
+    private float nextAttackTime = 0f;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +39,25 @@ public class Player_Movement : MonoBehaviour
         {
             Shoot();
             nextFireTime = Time.time + fireRate; // set next available fire time
+        } else if (Input.GetMouseButton(1) && Time.time >= nextAttackTime)
+        {
+            Melee();
+            nextAttackTime = Time.time + attackRate;
         }
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
         rb2d.velocity = new Vector2(moveHorizontal * speed, moveVertical * speed);
+
+
+        //animator movement info
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(moveX, moveY).normalized;
+
+        animator.SetFloat("MoveX", movement.x);
+        animator.SetFloat("MoveY", movement.y);
+        animator.SetBool("Speed", 0 < movement.sqrMagnitude);
     }
 
     void Shoot()
@@ -48,5 +76,31 @@ public class Player_Movement : MonoBehaviour
 
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         bullet.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+    }
+
+    void Melee()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        Vector2 direction = (mousePos - transform.position).normalized;
+        Vector2 attackPos = (Vector2)transform.position + direction * meleeRange;
+
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPos, meleeRadius);
+
+    }
+    void OnDrawGizmosSelected()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePos.z = 0f;
+        Vector2 direction = (mousePos - transform.position).normalized;
+        Vector2 attackPos = (Vector2)transform.position + direction * meleeRange;
+
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos, meleeRadius);
+    }
+
+    public void take_damage(float damage)
+    {
+        player_health = player_health - damage;
     }
 }
